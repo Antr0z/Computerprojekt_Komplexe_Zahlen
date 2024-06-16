9390f5401432caaee1705c825b1ebc90ccde9f2c
Antr0z <mr.antroz@gmail.com>
2024-06-16 19:47:06 +0200

    Letzter Tag fertig

--- cmplxpol_s86689.cpp
+++ cmplxpol_s86689.cpp
@@ -13,38 +13,38 @@ cmplxpol::cmplxpol(void){ //Standart-Konstruktor
     phrad = 0.0;
 }
 
-cmplxpol::cmplxpol(double betrag, double phase){ //parametisierbarer Konstruktor für Betrag und Phase
-    while(phase > 180 || phase < -180){ //while loop um Phase zwischen -180° und 180° zu bringen
-        if(phase > 180){
-            phase = phase - 360;
-        }
-        if(phase < -180){
-            phase = phase + 360;
+cmplxpol::cmplxpol(double betrag, double phase){    //parametisierbarer Konstruktor für Betrag und Phase
+    while(phase > 180 || phase < -180){             //while loop um Phase zwischen -180° und 180° zu bringen
+        if(phase > 180){                            //entweder 360° abziehen wenn es größer als 180° ist oder 360° hinzufügen wenn es kleiner ist als 180°
+            phase = phase - 360;        
+        }                               
+        if(phase < -180){               
+            phase = phase + 360;        
         }
     }
     
-    bt = sqrt(betrag * betrag);
+    bt = sqrt(betrag * betrag);                     //hier wird der Betrag auf eine positive Stelle gebracht
     
-    if(betrag != sqrt(betrag * betrag)){
+    if(betrag != sqrt(betrag * betrag)){            //korigieren der Phase wenn der Betrag negativ war
         ph = phase - 180;
         if(phase < -180){
             phase = phase + 360;
         }  
-    }else ph = phase;
+    }else ph = phase;                               //sonst wird der Wert einfach übernommen
 
-    double radiant = ph * (M_PI/180);
+    double radiant = ph * (M_PI/180);               //umrechnung von Gradmaß in Radiant für Trigonometrie funktionen
     re = bt * cos(radiant);
     im = bt * sin(radiant);
     phrad = radiant;
     }
 
 cmplxpol::cmplxpol(double real,const char cmplxj , double imag):re(real), im(imag){ //parametisierbarer Konstruktor für Real- und Imaginärteil  
-                                                                                   //Der char zum unterscheiden vom anderem Konstruktor
-    bt = sqrt(real*real+imag*imag); 
+                                                                                    //Der char zum unterscheiden vom anderem Konstruktor
+    bt = sqrt(real*real+imag*imag);                                                 //berechnung des Betrags
 
-    double radiant = atan(imag/real);
+    double radiant = atan(imag/real);                                               //Vorberechnung der Phase in radiant  
 
-    if (real < 0.0){
+    if (real < 0.0){                                                                //Fallentscheidung für das richtige berechnen von der Phase
         if (imag >= 0.0){
             if(real == 0.0 && imag == 0.0){
                 radiant = 0.0;
@@ -63,8 +63,8 @@ cmplxpol::cmplxpol(double real,const char cmplxj , double imag):re(real), im(ima
             radiant = radiant - M_PI;
         }
     }
-    ph = radiant * (180/M_PI);
-    phrad = radiant;
+    ph = radiant * (180/M_PI);                                                      //umrechnung der Phase in Grad und Zuweisung
+    phrad = radiant;                                                                //zuweisung der Phase in Radiant
 }
 
 /**** Methoden ****/
@@ -88,7 +88,7 @@ double cmplxpol::imag(void) const{ //Rückgabe des Imaginärteils aus den Attrib
     return im;
 }
 
-const char cmplxpol::imagj(void) const{
+const char cmplxpol::imagj(void) const{ //Methode für Imaginären char um den Konstruktor bei manchen Funktionen Aufzurufen(habe es nicht besser hinbekommen)
     const char* j = "j";
     return *j;
 }
@@ -106,7 +106,7 @@ double cmplxpol::arg_rad(void) const{ //Rückgabe der Phase(rad) aus den Attribu
 }
 
 void cmplxpol::set_pol(double betrag, double phase){ //set_pol methode die alle Attribute nochmal neu berechnet
-    while(phase > 180 || phase < -180){ //while loop um Phase zwischen -180° und 180° zu bringen
+    while(phase > 180 || phase < -180){ //Genau gleiches vorgehen wie beim Konstruktor für Betrag und Phase
         if(phase > 180){
             phase = phase - 360;
         }
@@ -122,17 +122,17 @@ void cmplxpol::set_pol(double betrag, double phase){ //set_pol methode die alle 
         if(phase < -180){
             phase = phase + 360;
         }  
-    }else ph = phase;
-     //zuweisung der phase am ende des loops
+    }else ph = phase;       
 
+     
     double radiant = ph * (M_PI/180);
     re = bt * cos(radiant);
     im = bt * sin(radiant);
     phrad = radiant;
     }
 
-void cmplxpol::set_pol(double real, const char cmplxj, double imag){
-    im = imag;
+void cmplxpol::set_pol(double real, const char cmplxj, double imag){    //setzen einer neuen Komplexen Zahl mit Betrag und Phase
+    im = imag;                                                          //genau gleiches vorgehen wie beim Real- und Imaginärteil Konstruktor
     re = real;
     bt = sqrt(real*real+imag*imag); 
 
@@ -161,7 +161,7 @@ void cmplxpol::set_pol(double real, const char cmplxj, double imag){
     phrad = radiant;
 }
 
-void cmplxpol::set_pol(const cmplxpol& z){
+void cmplxpol::set_pol(const cmplxpol& z){                              //überladung der zwei Methoden
     ph = z.ph;
     bt = z.bt;
     re = z.re;
@@ -169,16 +169,16 @@ void cmplxpol::set_pol(const cmplxpol& z){
     phrad = z.phrad;
     } 
 
-void cmplxpol::drucke(const cmplxpol& z){
+void cmplxpol::drucke(const cmplxpol& z){                               //Methode die einfdach nur den Ausgabeoperator wiedergibt
     std::cout << z;
 }
 
 /**** Überladung der Operatoren ****/
 
-std::ostream& operator << (std::ostream& p, const cmplxpol& z){ 
+std::ostream& operator << (std::ostream& p, const cmplxpol& z){         //überladung des Ausgabe Operators
     double zph = z.arg();
-    const char* zvor = "";
-    if(zph < 0){
+    const char* zvor = "";                                              
+    if(zph < 0){                                                        //Überprüfung ob ein Vorzeichen nötig ist
         zvor = "-";
     }
     ;
@@ -188,41 +188,41 @@ std::ostream& operator << (std::ostream& p, const cmplxpol& z){
 }
 
 
-cmplxpol operator~(const cmplxpol& z){
-    return cmplxpol(z.re, z.imagj(), -z.im);
+cmplxpol operator~(const cmplxpol& z){                                  //überladeung des ~ Operators um Konjugiert komplexe Zahlen Wiederzugeben
+    return cmplxpol(z.re, z.imagj(), -z.im);                            //hier wird auch noch der Konstruktor aufgerufen damit gelich alles umgerechnet wird(deswegen auch die imagj() Methode)
 }
 
-cmplxpol operator+(const cmplxpol& z1, const cmplxpol& z2){
+cmplxpol operator+(const cmplxpol& z1, const cmplxpol& z2){             //Additions Operator
     cmplxpol z3;
     z3.re = z1.re + z2.re;
     z3.im = z1.im + z2.im;
     return cmplxpol(z3.re,z3.imagj(),z3.im);
 }
 
-cmplxpol operator-(const cmplxpol& z1, const cmplxpol& z2){
+cmplxpol operator-(const cmplxpol& z1, const cmplxpol& z2){             //Subtraktions Operator
     cmplxpol z3;
     z3.re = z1.re - z2.re;
     z3.im = z1.im - z2.im;
     return cmplxpol(z3.re,z3.imagj(),z3.im);
 }
 
-cmplxpol operator*(const cmplxpol& z1, const cmplxpol& z2){
+cmplxpol operator*(const cmplxpol& z1, const cmplxpol& z2){             //Multiplikations Operator
     cmplxpol z3;
     z3.re = z1.re*z2.re-z1.im*z2.im;
     z3.im = z1.re*z2.im+z1.im*z2.re;
     return cmplxpol(z3.re,z3.imagj(),z3.im);
 }
 
-cmplxpol operator/(const cmplxpol& z1, const cmplxpol& z2){
+cmplxpol operator/(const cmplxpol& z1, const cmplxpol& z2){             //Divisions Operator
     cmplxpol z3;
     z3.re = (z1.re*z2.re-z1.im*z2.im)/(z2.re*z2.re+z2.im*z2.im);
     z3.im = (z1.re*z2.im+z1.im*z2.re)/(z2.re*z2.re+z2.im*z2.im);
     return cmplxpol(z3.re,z3.imagj(),z3.im); 
 }
 
-double operator+=(cmplxpol& z1, const cmplxpol z2){
-    z1.re = z1.re + z2.re;
-    z1.im = z1.im + z2.im;
+double operator+=(cmplxpol& z1, const cmplxpol z2){                     //so gut wie das gleiche vorgehen wie beim Additions Operators nur hier wollte der Konstruktor nicht 
+    z1.re = z1.re + z2.re;                                              //deswegen habe ich alles nochmal berechnet wie im Konstruktor
+    z1.im = z1.im + z2.im;                                              //das gleiche Problem ist auch mit den anderen Operatoren
     double imag = z1.im;
     double real = z1.re;
     z1.bt = sqrt(real*real+imag*imag); 
@@ -253,7 +253,7 @@ double operator+=(cmplxpol& z1, const cmplxpol z2){
     return (z1.re,z1.im,z1.ph,z1.bt,z1.phrad);
 }
 
-double operator-=(cmplxpol& z1, const cmplxpol z2){
+double operator-=(cmplxpol& z1, const cmplxpol z2){                     //so gut wie das gleiche vorgehen wie beim Subtraktions Operators
     z1.re = z1.re - z2.re;
     z1.im = z1.im - z2.im;
     double imag = z1.im;
@@ -286,7 +286,7 @@ double operator-=(cmplxpol& z1, const cmplxpol z2){
     return (z1.re,z1.im,z1.ph,z1.bt,z1.phrad);
 }
 
-double operator*=(cmplxpol& z1, const cmplxpol z2){
+double operator*=(cmplxpol& z1, const cmplxpol z2){                     //so gut wie das gleiche vorgehen wie beim Multiplikations Operators
     z1.re = z1.re*z2.re-z1.im*z2.im;
     z1.im = z1.re*z2.im+z1.im*z2.re;
     double imag = z1.im;
@@ -319,7 +319,7 @@ double operator*=(cmplxpol& z1, const cmplxpol z2){
     return (z1.re,z1.im,z1.ph,z1.bt,z1.phrad);
 }
 
-double operator/=(cmplxpol& z1, const cmplxpol z2){
+double operator/=(cmplxpol& z1, const cmplxpol z2){                     //so gut wie das gleiche vorgehen wie beim Multiplikations Operators
     z1.re = (z1.re*z2.re-z1.im*z2.im)/(z2.re*z2.re+z2.im*z2.im);
     z1.im = (z1.re*z2.im+z1.im*z2.re)/(z2.re*z2.re+z2.im*z2.im);
     double imag = z1.im;
@@ -352,53 +352,53 @@ double operator/=(cmplxpol& z1, const cmplxpol z2){
     return (z1.re,z1.im,z1.ph,z1.bt,z1.phrad);
 }
 
-cmplxpol operator+(const cmplxpol& z){
+cmplxpol operator+(const cmplxpol& z){                                  //Präfix für das Pluszeichen
     return(z);
 }
 
-cmplxpol operator-(const cmplxpol& z){
-    return(-z.re, -z.im, -z.bt, -z.phrad, -z.ph);
+cmplxpol operator-(const cmplxpol& z){                                  //Präfix um Komplexe Zahl negativ zu machen 
+    return cmplxpol(-1*z.re,z.imagj() , -1*z.im);                       //der ausgabe operator verträgt sich damit nicht weil aus irgendeinem grund der Konvertierungskonstruktor aufgerufen wird
 }
 
-cmplxpol operator++(const cmplxpol& z){
+cmplxpol operator++(const cmplxpol& z){                                 //Addition einer Einheit
     return cmplxpol(1 + z.re, z.imagj(), 1 + z.im);
 }
 
-cmplxpol cmplxpol::operator++(int){
+cmplxpol cmplxpol::operator++(int){                                     //Addition einer Einheit als Postfix
     cmplxpol z = *this;
     return cmplxpol(z.re + 1,z.imagj(),z.im + 1);    
 }
 
-cmplxpol operator--(const cmplxpol& z){
+cmplxpol operator--(const cmplxpol& z){                                 //subtaktion einer einheit
     return cmplxpol(-1 + z.re, z.imagj(), -1 + z.im);
 }
 
-cmplxpol cmplxpol::operator--(int){
+cmplxpol cmplxpol::operator--(int){                                     //subtraktion einer einheit als Postfix
     cmplxpol z = *this;
     return cmplxpol(z.re - 1,z.imagj(),z.im - 1);    
 }
 
-bool operator==(const cmplxpol& z1, const cmplxpol& z2){
+bool operator==(const cmplxpol& z1, const cmplxpol& z2){                //Vergleich zweier Komplexer Zahlen
     if(z1.bt == z2.bt && z1.ph == z2.ph){
         return bool(true);
     }else
         return bool(false);
 }
 
-bool operator!=(const cmplxpol& z1, const cmplxpol& z2){
+bool operator!=(const cmplxpol& z1, const cmplxpol& z2){                //ungleich vergleich
     if(z1.bt != z2.bt || z1.ph != z2.ph){
         return bool(true);
     }else
         return bool(false);
 }
-bool operator<(const cmplxpol& z1, const cmplxpol& z2){
+bool operator<(const cmplxpol& z1, const cmplxpol& z2){                 //kleiner als vergleich
     if(z1.bt < z2.bt){
         return bool(true);
     }else
         return bool(false);
 }
 
-bool operator>(const cmplxpol& z1, const cmplxpol& z2){
+bool operator>(const cmplxpol& z1, const cmplxpol& z2){                 //größer als vergleich
     if(z1.bt > z2.bt){
         return bool(true);
     }else
--- cmplxpol_s86689.h
+++ cmplxpol_s86689.h
@@ -3,51 +3,86 @@
 #include <iostream>
 class cmplxpol{
     private:
-        double bt, ph, re, im, phrad;
-    public:
+        double bt, ph, re, im, phrad;   //alle Attribute die ich gebrauchen kann  
+                                        //ich habe Real- ,Imaginärteil und Phase in Radiant mit hinzugefügt weil mir das teilweise was gespart hat
+    public:                              
 
         /**** Konstruktoren ****/
 
         cmplxpol(void); //Standart-Konstruktor
-        cmplxpol(double betrag, double phase); //Parametisierbarer Konstruktor für Betrag und Phase
-        cmplxpol(double real,const char complxj, double imag); //Parametisierbarer Konstruktor für Real- und Imaginärteil
+
+        cmplxpol(double betrag, double phase);  //Parametisierbarer Konstruktor für Betrag und Phase
+
+        cmplxpol(double real,const char complxj, double imag);  //Parametisierbarer Konstruktor für Real- und Imaginärteil
+
         cmplxpol(double i); //Konvertierungs-Konstruktor double -> cmplxpol        
-        operator double (void) const; //Konvertierungs-Konstruktor cmplxpol -> double
+
+        operator double (void) const;   //Konvertierungs-Konstruktor cmplxpol -> double
         
         /**** Methoden ****/
         
-        double real(void) const;
-        double imag(void) const;
-        const char imagj(void) const;
-        double abs(void) const;
-        double arg(void) const;
-        double arg_rad(void) const;
-        void set_pol(double betrag, double phase);
-        void set_pol(double real,const char complxj, double imag);
-        void set_pol(const cmplxpol& z);
-        void drucke(const cmplxpol& z);
+        double real(void) const;                                    //Wiedergabe des Realteils
+
+        double imag(void) const;                                    //Wiedergabe des Imaginärteils
+
+        const char imagj(void) const;                               //Eine Methode für den komplexen buchstaben j (habe diese methode hinzugefügt damit ich den
+                                                                    //Konstruktor bei manchen Methoden ausführen kann)
+
+        double abs(void) const;                                     //Methode für den Betrag 
+
+        double arg(void) const;                                     //Methode für die Phase 
+
+        double arg_rad(void) const;                                 //Methode für die Phase in Radiant
+
+        void set_pol(double betrag, double phase);                  //setzen einer neuen Komplexen Zahl mit Betrag und Phase 
+
+        void set_pol(double real,const char complxj, double imag);  //setzen einer neuen Komplexen Zahl mit Real- und Imaginärteil
+
+        void set_pol(const cmplxpol& z);                            //Überladung der beiden set_pol Methoden damit beide angenommen werden können
+
+        void drucke(const cmplxpol& z);                             //wiedergabe von betrag und phase
 
         /**** Überladene Operatoren ****/
 
-        friend std::ostream& operator<<(std::ostream& p, const cmplxpol& z);
-        friend cmplxpol operator~(const cmplxpol& z);
-        friend cmplxpol operator+(const cmplxpol& z1, const cmplxpol& z2);
-        friend cmplxpol operator-(const cmplxpol& z1, const cmplxpol& z2);
-        friend cmplxpol operator*(const cmplxpol& z1, const cmplxpol& z2);
-        friend cmplxpol operator/(const cmplxpol& z1, const cmplxpol& z2);
-        friend double operator+=(cmplxpol& z1, const cmplxpol z2);
-        friend double operator-=(cmplxpol& z1, const cmplxpol z2);
-        friend double operator*=(cmplxpol& z1, const cmplxpol z2);
-        friend double operator/=(cmplxpol& z1, const cmplxpol z2);
-        friend cmplxpol operator+(const cmplxpol& z);
-        friend cmplxpol operator-(const cmplxpol& z);
-        friend cmplxpol operator++(const cmplxpol& z);
-        cmplxpol operator++(int);
-        cmplxpol operator--(int);
-        friend cmplxpol operator--(const cmplxpol& z);
-        friend bool operator==(const cmplxpol& z1, const cmplxpol& z2);
-        friend bool operator!=(const cmplxpol& z1, const cmplxpol& z2);
-        friend bool operator<(const cmplxpol& z1, const cmplxpol& z2);
-        friend bool operator>(const cmplxpol& z1, const cmplxpol& z2);
+        friend std::ostream& operator<<(std::ostream& p, const cmplxpol& z);    //überladung des Ausgabe Operators 
+                                                                                //(ich habe fast überall friend mit dazu geschrieben weil es meistens besser funktioniert hat)
+
+        friend cmplxpol operator~(const cmplxpol& z);                           //überladeung des ~ Operators um Konjugiert komplexe Zahlen Wiederzugeben
+
+        friend cmplxpol operator+(const cmplxpol& z1, const cmplxpol& z2);      //Überladung des Plus Operators
+
+        friend cmplxpol operator-(const cmplxpol& z1, const cmplxpol& z2);      //Überladung des Minus Operators
+
+        friend cmplxpol operator*(const cmplxpol& z1, const cmplxpol& z2);      //Überladung des Mal Operators
+
+        friend cmplxpol operator/(const cmplxpol& z1, const cmplxpol& z2);      //Überladung des Geteilt Operators
+
+        friend double operator+=(cmplxpol& z1, const cmplxpol z2);              //Überladung des Plusgleich Operators
+
+        friend double operator-=(cmplxpol& z1, const cmplxpol z2);              //Überladung des Minusgleich Operators
+
+        friend double operator*=(cmplxpol& z1, const cmplxpol z2);              //Überladung des Malgleich Operators
+
+        friend double operator/=(cmplxpol& z1, const cmplxpol z2);              //Überladung des Geteiltgleich Operators
+
+        friend cmplxpol operator+(const cmplxpol& z);                           //Überladung des Plus Operators als Präfix
+
+        friend cmplxpol operator-(const cmplxpol& z);                           //Überladung des Minus Operators als Präfix
+
+        friend cmplxpol operator++(const cmplxpol& z);                          //Überladung des ++ Operator als Präfix
+        cmplxpol operator++(int);                                               //und Postfix
+                                                               
+        friend cmplxpol operator--(const cmplxpol& z);                          //Überladung des -- Operator als Präfix
+        cmplxpol operator--(int);                                               //und Postfix
+
+        //Vergleichsoperatoren
+
+        friend bool operator==(const cmplxpol& z1, const cmplxpol& z2);         //Überladung des Vergleichs Operators
+
+        friend bool operator!=(const cmplxpol& z1, const cmplxpol& z2);         //Überladung des Ungleich Operators
+
+        friend bool operator<(const cmplxpol& z1, const cmplxpol& z2);          //Überladung des kleiner als Operators  
+
+        friend bool operator>(const cmplxpol& z1, const cmplxpol& z2);          //Überladung des größer als Operators  
 };
 #endif
